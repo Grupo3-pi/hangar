72c3f7d244d252f62595d6be0bda179646417d94
Caio-CGodoy <2109745@aluno.univesp.br>
2023-04-11 23:48:16 -0300

    Configuração do DB
    Configuração do banco de dados

--- cardapio/admin.py
+++ cardapio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Cardapio, Prato, Ingredientes
 
-# Register your models here.
+admin.site.register(Cardapio)
+admin.site.register(Prato)
+admin.site.register(Ingredientes)
--- hangar252/settings.py
+++ hangar252/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'hangar252.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cardapio_db',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
